c4b9b31a03598650ea58e96eb80823c3ab041959
Ric-Oli <ricardooliveiraprivate@hotmail.com>
2025-12-04 14:33:04 +0100

    feat: Showing all answers for each take on the result-page of the exercise

--- src/controllers/ResultController.php
+++ src/controllers/ResultController.php
@@ -2,6 +2,7 @@
 
 namespace App\controllers;
 
+use App\Models\Field;
 use App\Models\Fulfillment;
 
 class ResultController
@@ -9,6 +10,8 @@ class ResultController
     public function showResults($id): array
     {
         $fulfillments = Fulfillment::getFulfillmentsForExercise($id);
+        $fields = Field::getByExerciseId($id);
+        $answers = Fulfillment::getAllAnswersForFulfillment($fulfillments);
 
         if (!$fulfillments) {
             return ['status_code' => 404, 'data' => ['title' => 'Not Found']];
@@ -17,7 +20,9 @@ class ResultController
         return [
             'view' => "views/exercises/show-results",
             'data' => [
-                'fulfillments' => $fulfillments
+                'fulfillments' => $fulfillments,
+                'answers' => $answers,
+                'fields' => $fields
             ]
         ];
     }
--- src/views/exercises/show-results.php
+++ src/views/exercises/show-results.php
@@ -2,13 +2,18 @@
     <thead>
         <tr>
             <th>Take</th>
-            <th><a href="#">Titre de la question (à remplir)</a></th>
+            <?php foreach($fields as $field){ ?>
+            <th><a href="#"><?= $field->label ?></a></th>
+            <?php } ?>
         </tr>
     </thead>
     <tbody>
     <?php foreach ($fulfillments as $fulfillment) { ?>
         <tr>
             <td><a href="#"><?= $fulfillment ? $fulfillment->timestamp : null ?></a></td>
+            <?php foreach ($answers[$fulfillment->id] as $answer) { ?>
+                <td><?= $answer ?></td>
+            <?php } ?>
         </tr>
      <?php } ?>
     </tbody>
